7cbcc1b7446ed4e2ef2e720e2d1bf44b56a0e0c1
RomainLabatut <romain_lab@hotmail.fr>
2021-02-15 22:55:35 +0100

    third commit

---
+++ pageForm.js
@@ -0,0 +1,95 @@
+const dataPraticien = [
+    {
+        "id": 1,
+        "nom": "Lee",
+        "prenom": "Bruce",
+        "adresse": "Marseille",
+        "specialite": "ophtalmologue",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 2,
+        "nom": "Hugo",
+        "prenom": "Victor",
+        "adresse": "Aix-En-Provence",
+        "specialite": "Dentiste",
+        "photo": "PraticienF.jfif"
+    },
+    {
+        "id": 3,
+        "nom": "Mozart",
+        "prenom": "Amadeus",
+        "adresse": "Paris",
+        "specialite": "Dentiste",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 4,
+        "nom": "Beethoven",
+        "prenom": "Ludwig Von",
+        "adresse": "Nantes",
+        "specialite": "ophtalmologue",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 5,
+        "nom": "Proust",
+        "prenom": "Marcel",
+        "adresse": "Marseille",
+        "specialite": "Dentiste",
+        "photo": "PraticienF.jfif"
+    },
+    {
+        "id": 6,
+        "nom": "Arouet",
+        "prenom": "François-Marie",
+        "adresse": "Paris",
+        "specialite": "ophtalmologue",
+        "photo": "PraticienF.jfif"
+    },
+    {
+        "id": 7,
+        "nom": "Poquelin",
+        "prenom": "Jean-baptiste",
+        "adresse": "Lille",
+        "specialite": "Dentiste",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 8,
+        "nom": "Shakespear",
+        "prenom": "William",
+        "adresse": "Paris",
+        "specialite": "Infirmier",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 9,
+        "nom": "Geldog",
+        "prenom": "Bob",
+        "adresse": "Rouen",
+        "specialite": "Infirmier",
+        "photo": "PraticienH.jfif"
+    },
+    {
+        "id": 10,
+        "nom": "Shakespear",
+        "prenom": "William",
+        "adresse": "Lille",
+        "specialite": "Dentiste",
+        "photo": "PraticienH.jfif"
+    }
+];
+
+let specialite = document.querySelector('#specialite');
+
+const bouton = document.querySelector('.bouton');
+
+bouton.addEventListener('click', ()=>{
+    let searching = specialite.Value;
+    dataPraticien.forEach((element) =>{
+        
+
+    });
+
+});
